830e4fc39f0799657ad0f51a087f8532d9923668
Kira Yatsishina <yatsishinakira@gmail.com>
2024-12-26 12:06:11 +0200

    first commit

---
+++ trainers-workload/src/main/java/micro/trainersworkload/TrainersWorkloadApplication.java
@@ -0,0 +1,13 @@
+package micro.trainersworkload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainersWorkloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainersWorkloadApplication.class, args);
+    }
+
+}
---
+++ trainers-workload/src/test/java/micro/trainersworkload/TrainersWorkloadApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.trainersworkload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainersWorkloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
